7257f45a3cffdbba3f641ff4fb7874a519e53ba9
Aaron F Stanton <afstanton@gmail.com>
2023-05-25 13:56:05 -0400

    Initial commit

---
+++ lib/pcgen_ruby.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "pcgen_ruby/version"
+
+module PcgenRuby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pcgen_ruby/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module PcgenRuby
+  VERSION = "0.0.1"
+end
